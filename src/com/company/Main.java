05c71ab5421f6f0c21be481083590d37d076031d
Prachi <prachipragnyapadhi@gmail.com>
2022-09-17 05:52:50 +0530

    commiting try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,25 @@
 package com.company;
+import java.util.*;
+// dragon problem codeforces
+public class Main
+{
+        public static void main(String[] args)
+        {
+            Scanner s=new Scanner(System.in);
+            int k=s.nextInt();
+            int l=s.nextInt();
+            int m=s.nextInt();
+            int n=s.nextInt();
+            int d=s.nextInt();
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello github");
+            int c=0;
+            for(int i=1;i<=d;i++)
+            {
+                if(i%k==0 || i%l==0 || i%m==0 || i%n==0)
+                {
+                    c++;
+                }
+            }
+            System.out.println(c);
+        }
     }
-}
