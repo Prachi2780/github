6ebcf22ca0092d37fa329debc03fe3f68793e594
Prachi <prachipragnyapadhi@gmail.com>
2022-09-17 06:14:19 +0530

    commiting try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,38 @@
 package com.company;
 import java.util.*;
-// dragon problem codeforces
+// beautiful matrix codeforces
 public class Main
 {
-        public static void main(String[] args)
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int[][] arr=new int[5][5];
+        for(int i1=0;i1<5;i1++)
         {
-            Scanner s=new Scanner(System.in);
-            int[][] arr=new int[5][5];
-            for(int i1=0;i1<5;i1++)
+            for(int j1=0;j1<5;j1++)
             {
-                for(int j1=0;j1<5;j1++)
-                {
-                    arr[i1][j1]=s.nextInt();
-                }
+                arr[i1][j1]=s.nextInt();
             }
-            System.out.println(index(arr));
         }
-
-        static int index(int[][] arr)
-        {
-            int max=0;
-            int c=0;
-            for(int i=0;i<5;i++)
-            {
-                for(int j=0;j<5;j++)
-                {
-
-                    if(arr[i][j]>max)
-                    {
-                        c=Math.abs(2-i)+Math.abs(2-j);
-                        break;
-                    }
-                }
-            }
-            return c;
-        }
+        System.out.println(index(arr));
     }
+
+    static int index(int[][] arr)
+    {
+        int max=0;
+        int c=0;
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+
+                if(arr[i][j]>max)
+                {
+                    c=Math.abs(2-i)+Math.abs(2-j);
+                    break;
+                }
+            }
+        }
+        return c;
+    }
+}
